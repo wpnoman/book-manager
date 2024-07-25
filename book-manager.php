93ade9059539159cb6816db3511863eb541d0e34
sbnoman01 <sbnoman27@gmail.com>
2024-07-25 21:33:15 +0600

    clean

--- book-manager.php
+++ book-manager.php
@@ -39,84 +39,10 @@ require BKM_INCLUDE_PATH . '/book-manager.php';
 require BKM_INCLUDE_PATH . '/admin/Admin.php';
 
 
-/**
- * Main ADAE Class
- *
- * The init class that runs the Hello World plugin.
- * Intended To make sure that the plugin's minimum requirements are met.
- *
- * You should only modify the constants to match your plugin's needs.
- *
- * Any custom code should go inside Plugin Class in the plugin.php file.
- * @since 1.0.0
- */
-final class BKM_Final {
-
-	/**
-	 *
-	 * $instance property for instance
-	 */
-	private static $instance = null;
-
-	/**
-	 * Plugin Version
-	 *
-	 * @since 1.0.0
-	 * @var string The plugin version.
-	 */
-	const VERSION = '1.0.0';
-
-	/**
-	 * Plugin prefix
-	 *
-	 * @since 1.0.0
-	 * @var string The plugin version.
-	 */
-	const PREFIX = 'bkm_';
-
-	/**
-	 * Minimum PHP Version
-	 *
-	 * @since 1.0.0
-	 * @var string Minimum PHP version required to run the plugin.
-	 */
-	const MINIMUM_PHP_VERSION = '7.4';
-
-	/**
-	 * Constructor
-	 *
-	 * @since 1.0.0
-	 * @access public
-	 */
-	public function __construct() {
-
-		// Init Plugin
-		add_action( 'plugins_loaded', array( $this, 'plugins_loaded' ) );
-	}
-
-	/**
-	 *
-	 * @return \Instance
-	 * @since  1.0.0
-	 */
-	// public static function getInstance() {
-	// 	if ( ! isset( self::$instance ) ) {
-	// 		self::$instance = new self();
-	// 	}
-	// 	return self::$instance;
-	// }
-
-	public function plugins_loaded() {
-		// load_plugin_textdomain( 'tpebl', false, basename( dirname( __FILE__ ) ) . '/lang' );
-		Book_manager\Book_manager::getInstance();
-	}
-
-
-}
-
 if ( ! function_exists( 'BKM_Init' ) ) {
 	function BKM_init() {
-		return new BKM_Final();
+		// return Book_manager\Book_manager::getInstance();
+		// return new \Book_manager\Book_manager();
 	}
 	bkm_Init();
 }
